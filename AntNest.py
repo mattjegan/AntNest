cfdd0ff4e42f7822eb1fb08e4f47cce0de44641f
Matthew Egan <matthewj.egan@hotmail.com>
2014-11-07 12:18:28 +1100

    Added template code to AntNest.py

---
+++ AntNest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+## AntNest.py
+##
+## Written by Matthew Egan
+## License: MIT License
+##
+## https://github.com/tolo137/AntNest
+
+import pygame
+from pygame.locals import *
+
+def main():
+    pass
+
+if __name__ == "__main__": main()
